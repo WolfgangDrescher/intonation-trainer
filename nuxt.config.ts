c74ae8ac2c421bd0bf9bafee444800b4806d4358
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-03-04 22:03:33 +0100

    Add modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     srcDir: 'src/',
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/i18n-edge',
+        '@nuxt/content',
+    ],
 
 })
