b2084936de49182096153b64613e14f834381d3b
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-03-04 22:26:19 +0100

    Setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+const { purple } = require('tailwindcss/colors');
+
+module.exports = {
+    content: [
+        './src/layouts/**.{vue,html,js}',
+        './src/components/**/*.{vue,html,js}',
+        './src/pages/**/*.{vue,html,js}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: purple,
+            },
+        },
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                md: '3rem',
+                lg: '4rem',
+                xl: '5rem',
+                '2xl': '6rem',
+            },
+        },
+    },
+};
