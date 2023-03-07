fd6d8aa33801c6cd8e6f961bbed84614be39aafc
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-03-07 12:38:49 +0100

    Add IntonationChecker client component wrapper

---
+++ src/components/IntonationChecker.client.vue
@@ -0,0 +1,12 @@
+<script setup>
+import 'intonation-checker/style.css';
+import IntonationChecker from 'intonation-checker';
+import { createWorkerVerovioToolkit } from 'vue-verovio-canvas';
+
+const { verovioWorker } = useVerovioWorker();
+const toolkit = createWorkerVerovioToolkit(verovioWorker);
+</script>
+
+<template>
+    <IntonationChecker :toolkit="toolkit" />
+</template>
