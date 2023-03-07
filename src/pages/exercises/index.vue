28133ce07de1a3897abe161f017cf06282c64173
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-03-07 12:44:35 +0100

    Display exercises from nuxt content

--- src/pages/exercises/index.vue
+++ src/pages/exercises/index.vue
@@ -1,8 +1,21 @@
+<script setup>
+const { data } = await useAsyncData('exercises', () => queryContent('/exercises').find());
+</script>
+
 <template>
     <Container>
         <SectionContainer>
             <section>
                 <Heading>{{ $t('exercises') }}</Heading>
+                <ul>
+                    <template v-for="exercise in data" :key="exercise._id">
+                        <li v-for="variant in exercise.variants" :key="`${exercise._id}${variant.id}`">
+                            <NuxtLink :to="`${exercise._path}/${variant.id}`">
+                                {{ exercise.title }} / {{ variant?.title || `Variante ${variant.id}` }}
+                            </NuxtLink>
+                        </li>
+                    </template>
+                </ul>
             </section>
         </SectionContainer>
     </Container>
