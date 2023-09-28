f17848f023e968611c6b2f81494c0f094b1311d8
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-09-28 17:59:16 +0200

    Add play button component

---
+++ src/components/PlayButton.client.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { Howl } from 'howler';
+
+const props = defineProps({
+    url: String,
+    keyboardShortcuts: Boolean,
+});
+
+const audio = new Howl({
+    src: [props.url],
+    html5: true, // allow playing audio on muted phones
+});
+
+const isReady = ref(false);
+const isPlaying = ref(false);
+const seek = ref(0);
+
+if (audio.state() === 'loaded') {
+    isReady.value = true;
+}
+
+audio.on('load', () => {
+    // isReady.value = true;
+});
+
+audio.on('play', () => {
+    isPlaying.value = true;
+    requestAnimationFrame(updateLoop);
+});
+
+audio.on('end', () => {
+    isPlaying.value = false;
+});
+
+audio.on('pause', () => {
+    isPlaying.value = false;
+});
+
+audio.on('stop', () => {
+    isPlaying.value = false;
+});
+
+audio.on('seek', () => {
+    updateLoop();
+});
+
+function updateLoop() {
+    seek.value = audio.seek() || 0;
+    if (audio.playing()) {
+        requestAnimationFrame(updateLoop);
+    }
+}
+
+function toggle() {
+    if (audio.state() !== 'loaded') return;
+    if (audio.playing()) {
+        audio.pause();
+        audio.stop();
+    } else {
+        audio.play();
+    }
+}
+</script>
+
+<template>
+    <div @click="toggle">
+        <Icon v-if="isPlaying" name="heroicons:pause-circle-solid" />
+        <Icon v-else name="heroicons:play-circle-solid" />
+    </div>
+</template>
