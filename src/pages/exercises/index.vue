47e1a619cba7eae99eca4cc618c89163332a41dc
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-09-28 23:50:19 +0200

    Show one item in data table for each exercise and list variants within item

--- src/pages/exercises/index.vue
+++ src/pages/exercises/index.vue
@@ -4,24 +4,20 @@ const localePath = useLocalePath();
 const runtimeConfig = useRuntimeConfig();
 const { t } = useI18n();
 
-const tableItems = data.value.reduce((accumulator, currentItem) => {
-    currentItem.variants.forEach((variant) => {
-        accumulator.push({
-            _path: currentItem._path,
-            id: currentItem.id,
-            variantId: variant.id,
-            composer: currentItem.composer,
-            title: currentItem.variants.length === 1 ? currentItem.title : `${currentItem.title} – ${t('variantName', {name: variant.id})}`,
-            year: currentItem.year,
-            difficulty: currentItem.difficulty,
-            audioFilename: currentItem.audioFilename,
-            variantAudioFilename: variant.audioFilename,
-            instrumentation: currentItem.instrumentation,
-            sluggifyInstrumentation: sluggifyInstrumentation(currentItem.instrumentation),
-        });
-    });
-    return accumulator;
-}, []);
+const tableItems = data.value.map((item) => {
+    return {
+        _path: item._path,
+        id: item._path.split('/').pop(),
+        composer: item.composer,
+        title: item.title,
+        year: item.year,
+        difficulty: item.difficulty,
+        audioFilename: item.audioFilename,
+        instrumentation: item.instrumentation,
+        sluggifyInstrumentation: sluggifyInstrumentation(item.instrumentation),
+        variants: item.variants,
+    };
+});
 
 const tableHeaders = [
     { value: 'audio', text: '', align: 'center' },
@@ -54,14 +50,28 @@ function buildPath(fileName, exerciseId) {
                     </template>
                     <template #[`item.audio`]="{ item }">
                         <div class="text-center">
-                            <PlayButton :url="buildPath(item.audioFilename, item.variantId)"></PlayButton>
+                            <PlayButton :url="buildPath(item.audioFilename, item.id)"></PlayButton>
+                        </div>
+                    </template>
+                    <template #[`item.audio`]="{ item }">
+                        <div class="text-center">
+                            <PlayButton :url="buildPath(item.audioFilename, item.id)"></PlayButton>
                         </div>
                     </template>
                     <template #[`item.title`]="{ item }">
                         <!-- <NuxtLink :href="localePath({ name: 'exercises-id-variant', params: { id: item.id, variant: item.variantId } })"> -->
-                        <NuxtLink :to="`${item._path}/${item.variantId}`">
-                            {{ item.title }}
-                        </NuxtLink>
+                        <div>
+                            <NuxtLink :href="localePath({ name: 'exercises-id', params: { id: item.id } })">
+                                {{ item.title }}
+                            </NuxtLink>
+                        </div>
+                        <div v-if="item.variants.length > 1" class="text-xs flex gap-2">
+                            <div v-for="variant in item.variants">
+                                <NuxtLink :href="localePath({ name: 'exercises-id-variant', params: { id: item.id, variant: variant.id } })">
+                                    {{ $t('variantName', {name: variant.id}) }}
+                                </NuxtLink>
+                            </div>
+                        </div>
                     </template>
                     <template #[`item.difficulty`]="{ item }">
                         <Icon v-for="n in (item.difficulty ?? 0)" name="heroicons:star-solid" />
