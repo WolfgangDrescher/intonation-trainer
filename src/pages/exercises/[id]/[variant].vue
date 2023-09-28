96075245f98d04b7553452d532945563b0f98f74
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-09-28 23:48:29 +0200

    Show variant buttons

--- src/pages/exercises/[id]/[variant].vue
+++ src/pages/exercises/[id]/[variant].vue
@@ -1,5 +1,6 @@
 <script setup>
 const { params } = useRoute();
+const localePath = useLocalePath();
 const { data: exercise } = await useAsyncData(`/exercises/${params.id}`, () => queryContent(`/exercises/${params.id}`).findOne());
 
 const variant = exercise.value.variants.find(v => String(v.id) === params.variant);
@@ -17,6 +18,11 @@ if (!variant) {
         <SectionContainer>
             <section>
                 <Heading>{{ exercise.title }}</Heading>
+                <div v-if="exercise.variants.length > 1" class="flex gap-4 my-4">
+                    <VariantButton v-for="_variant in exercise.variants" :active="variant.id === _variant.id" :href="localePath({ name: 'exercises-id-variant', params: { id: exercise.id, variant: _variant.id } })">
+                        {{ $t('variantName', { name: _variant.id }) }}
+                    </VariantButton>
+                </div>
                 <div class="lg:h-[1000px]">
                     <IntonationChecker
                         :url="`/api${exercise._path}/${variant.id}`"
