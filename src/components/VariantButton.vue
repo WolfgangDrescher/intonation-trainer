65f72e4800bb8c2f5472fc2becc75ed89002df5d
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-09-28 23:46:38 +0200

    Add VariantButton component

---
+++ src/components/VariantButton.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+    to: String,
+    active: Boolean,
+});
+</script>
+
+<template>
+    <NuxtLink :to="to" class="px-4 py-3 bg-primary-400 text-white rounded" :class="{'bg-primary-600': active}">
+        <slot></slot>
+    </NuxtLink>
+</template>
