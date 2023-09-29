706c83c42f44d4e8e8cd16e47cc335f4a669a06b
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-09-29 10:46:48 +0200

    Improve button styles

--- src/components/VariantButton.vue
+++ src/components/VariantButton.vue
@@ -6,7 +6,7 @@ defineProps({
 </script>
 
 <template>
-    <NuxtLink :to="to" class="px-4 py-3 bg-primary-400 text-white rounded" :class="{'bg-primary-600': active}">
+    <NuxtLink :to="to" class="px-4 py-3 border border-primary-500 rounded hover:ring-purple-200 hover:ring-4 hover:border-purple-300" :class="{'bg-primary-500 text-white': active, 'text-primary-500': !active}">
         <slot></slot>
     </NuxtLink>
 </template>
