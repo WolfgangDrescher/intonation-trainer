3c60c970c1714f8218b126a30814675733d2c59e
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-10-16 12:58:28 +0200

    Improve gap

--- src/components/SectionContainer.vue
+++ src/components/SectionContainer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col gap-8 sm:gap-12 md:gap-16 lg:gap-20 xl:gap-24">
+    <div class="flex flex-col gap-4 sm:gap-6 md:gap-8 lg:gap-10 xl:gap-12">
         <slot></slot>
     </div>
 </template>
