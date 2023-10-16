7b60e4001430100dbe268fce771cb74f20ca6f11
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-10-16 12:56:47 +0200

    Use rose as primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,19 @@
-import { purple } from 'tailwindcss/colors';
 import { fontFamily } from 'tailwindcss/defaultTheme';
+
+const rose = {
+    DEFAULT: '#E2007D',
+    50: '#FF9BD2',
+    100: '#FF86C9',
+    200: '#FF5DB7',
+    300: '#FF35A5',
+    400: '#FF0C92',
+    500: '#E2007D',
+    600: '#AA005E',
+    700: '#72003F',
+    800: '#3A0020',
+    900: '#020001',
+    950: '#000000'
+};
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -11,7 +25,7 @@ export default {
     theme: {
         extend: {
             colors: {
-                primary: purple,
+                primary: rose,
             },
             fontFamily: {
                 ...fontFamily,
