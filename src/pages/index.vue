f0fcfb72200e95a2480fddac3eb7b94e924c998f
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-10-16 09:30:06 +0200

    Use logo

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,9 +2,12 @@
     <Container>
         <SectionContainer>
             <section class="text-center">
-                <Heading>{{ $t('app.title') }}</Heading>
-                <p>{{ $t('app.title.description') }}</p>
-                <Subheading>{{ $t('app.subtitle') }}</Subheading>
+                <!-- <Heading>{{ $t('app.title') }}</Heading> -->
+                <div class="flex justify-center">
+                    <img src="@/assets/logo.svg" :alt="$t('app.title')" width="300" />
+                </div>
+                <p class="mb-6">{{ $t('app.title.description') }}</p>
+                <div class="my-6"><Subheading>{{ $t('app.subtitle') }}</Subheading></div>
                 <p>{{ $t('app.subtitle.description') }}</p>
             </section>
         </SectionContainer>
