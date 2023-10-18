21b0a76d7ec8e34cfc40ddc431a43907f5732f29
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-10-18 22:26:03 +0200

    Fix wrong color

--- src/components/VariantButton.vue
+++ src/components/VariantButton.vue
@@ -7,7 +7,7 @@ defineProps({
 </script>
 
 <template>
-    <NuxtLink :to="to" class="px-4 py-3 border border-primary-500 rounded hover:ring-purple-200 hover:ring-4 hover:border-purple-300" :class="{'bg-primary-500 text-white': active, 'text-primary-500': !active}">
+    <NuxtLink :to="to" class="px-4 py-3 border border-primary-500 rounded hover:ring-primary-200 hover:ring-4 hover:border-purple-300" :class="{'bg-primary-500 text-white': active, 'text-primary-500': !active}">
         <slot></slot>
         <div v-if="difficulty" class="ml-2 inline-block align-text-bottom">
             <Icon v-for="n in (difficulty ?? 0)" name="heroicons:star-solid" />
